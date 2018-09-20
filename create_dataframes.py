fa49ba3ca2069867b92eae047a522b64215b1776
Stajah Hoeflich <stajah.hoeflich@cgi.com>
2018-09-20 15:51:08 -0400

    added all tables in dvdrentals database to a list of dataframes

--- create_dataframes.py
+++ create_dataframes.py
@@ -2,15 +2,10 @@ import sqlite3
 import pandas as pd
 from connect import db
 
-
 def get_dataframes():
-  cursor = db.cursor()
-  cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-  tables = cursor.fetchall()
   dataframes = []
-  for table_name in tables:
-    table_name = table_name[0]
-    table = pd.read_sql_query("SELECT * from %s" % table_name, db)
-    dataframes.append(table)
+  for table_name in db.table_names():
+     table = pd.read_sql_query("SELECT * from %s" % table_name, db)
+     dataframes.append(table)
 
   return dataframes
--- use_data.py
+++ use_data.py
@@ -1,3 +1,9 @@
 from create_dataframes import get_dataframes
 
-print(get_dataframes())
+for idx, df in enumerate(get_dataframes()):
+  # we now have all of the tables in the database contained in a list of pandas dataframes
+  # we can iterate through this list and do whatever needs to be done with the data in 
+  # all of the dataframes
+  
+  print ('Dataframe ' + str(idx) + ':')
+  print (df.head(1))
