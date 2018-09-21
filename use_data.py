1ffdac513ba960c0c643ee2d48ba04f6b5cb264d
Stajah Lee Hoeflich <sh024615@ohio.edu>
2018-09-21 15:35:19 -0400

    remove import

--- use_data.py
+++ use_data.py
@@ -1,5 +1,4 @@
 from create_dataframes import get_dataframes
-import user_choices as user
 
 dfs = get_dataframes()
 print ('You now have a dictionary of dataframes called dfs with the following keys:\n' + str(dfs.keys()))
